c0436a95fc51841c1f84b23ea47b781a7b03de29
Ege Taslicay <met391@students.cs.ubc.ca>
2025-07-12 15:43:46 -0700

    added tests for Movie class

---
+++ src/test/model/TestMovie.java
@@ -0,0 +1,66 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+
+
+class TestMovie {
+    private Movie testMovieInterstellar;
+
+    @BeforeEach 
+    void runBefore() {
+        testMovieInterstellar = new Movie("Interstellar", "Sci-Fi", 4, "Nolan's best");
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("Interstellar", testMovieInterstellar.getTitle());
+        assertEquals("Sci-Fi", testMovieInterstellar.getGenre());
+        assertEquals(4, testMovieInterstellar.getRating());
+        assertEquals("Nolan's best", testMovieInterstellar.getComment());
+    }
+
+    @Test
+    void testGetTitle() {
+        assertEquals("Interstellar", testMovieInterstellar.getTitle());
+    }
+
+    @Test
+    void testGetGenre() {
+        assertEquals("Sci-Fi", testMovieInterstellar.getGenre());
+    }
+
+    @Test
+    void testGetRating() {
+        assertEquals(4, testMovieInterstellar.getRating());
+    }
+
+    @Test
+    void testGetComment() {
+        assertEquals("Nolan's best", testMovieInterstellar.getComment());
+    }
+
+    @Test
+    void testSetRating() {
+        testMovieInterstellar.setRating(2);
+        assertEquals(2, testMovieInterstellar.getRating());
+    }
+
+    @Test
+    void testSetComment() {
+        testMovieInterstellar.setComment("Masterpiece");
+        assertEquals("Masterpiece", testMovieInterstellar.getComment());
+    }
+
+    @Test
+    void testToString() {
+        String result = testMovieInterstellar.toString();
+        assertTrue(result.contains("Interstellar"));
+        assertTrue(result.contains("Sci-Fi"));
+        assertTrue(result.contains("4"));
+        assertTrue(result.contains("Nolan's best"));
+    }
+}
